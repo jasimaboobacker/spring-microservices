dfba323e81a2df70cbf5add995410930e945f733
aboobackerj <jasim.aboobacker@kpn.com>
2020-06-14 15:59:58 +0200

    Spring cloud configurations

---
+++ limits-service/src/test/java/com/giantdiplodocus/microservice/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giantdiplodocus.microservice.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

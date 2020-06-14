7163490483a3e3bc48151361a2274ce14ff8f6bf
aboobackerj <jasim.aboobacker@kpn.com>
2020-06-14 23:25:04 +0200

    Eureka and ribbon configurations

---
+++ netflix-eureka-naming-server/src/test/java/com/giantdiplodocus/microservice/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giantdiplodocus.microservice.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

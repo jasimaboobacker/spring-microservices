0b00644f44cc132b6f5a35d66aa5394602cd4b7c
aboobackerj <jasim.aboobacker@kpn.com>
2020-06-14 22:15:01 +0200

    Feign configuration

--- currency-conversion-service/src/main/java/com/giantdiplodocus/microservice/currencyconversionservice/CurrencyConversionBean.java
+++ currency-conversion-service/src/main/java/com/giantdiplodocus/microservice/currencyconversionservice/CurrencyConversionBean.java
@@ -1,39 +1,28 @@
-package com.giantdiplodocus.microservice.currencyexchangeservice;
+package com.giantdiplodocus.microservice.currencyconversionservice;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import java.math.BigDecimal;
 
-@Entity
-public class ExchangeValue {
+public class CurrencyConversionBean {
 
-    @Id
-    @GeneratedValue
     private Long id;
-
-    @Column(name = "currency_from")
     private String from;
-
-    @Column(name = "currency_to")
     private String to;
-
     private BigDecimal conversionMultiple;
-
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
     private int port;
 
-    public ExchangeValue() {
+    public CurrencyConversionBean() {
     }
 
-    public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+    public CurrencyConversionBean(Long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity, BigDecimal totalCalculatedAmount, int port) {
         this.id = id;
         this.from = from;
         this.to = to;
         this.conversionMultiple = conversionMultiple;
+        this.quantity = quantity;
+        this.totalCalculatedAmount = totalCalculatedAmount;
+        this.port = port;
     }
 
     public Long getId() {
@@ -66,6 +55,22 @@ public class ExchangeValue {
 
     public void setConversionMultiple(BigDecimal conversionMultiple) {
         this.conversionMultiple = conversionMultiple;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getTotalCalculatedAmount() {
+        return totalCalculatedAmount;
+    }
+
+    public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+        this.totalCalculatedAmount = totalCalculatedAmount;
     }
 
     public int getPort() {
